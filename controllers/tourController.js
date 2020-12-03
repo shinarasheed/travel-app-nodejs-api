cec0dc3229532d47cca48da2f72fe75b5a9b0a98
shina <rasheedshinaopeyemi@gmail.com>
2020-12-03 09:17:49 +0100

    added eslint config and prettier config to enforced some runs an d format code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,26 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours.json`)
 );
 
+exports.checkD = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.validateData = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'name and price is required',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
