0b4f083184881808b4c578a31572047c38abb3a8
shina <rasheedshinaopeyemi@gmail.com>
2020-12-03 19:25:16 +0100

    adding more stuffs

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours.json`)
-);
+const Tour = require('../models/tourModel');
 
 exports.checkD = (req, res, next, val) => {
   if (req.params.id * 1 > tours.length) {
@@ -32,39 +28,21 @@ exports.getAllTours = (req, res) => {
   });
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => (el.id = id));
-  if (!tour) {
-    return res.status(404).json({
-      status: 'error',
-      message: 'Tour not found',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
-};
+exports.getTour = (req, res) => {};
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = new Tour({
+    name: 'forest hiker',
+    rating: 4.1,
+    price: 497,
+  });
 
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'sucess',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  newTour
+    .save()
+    .then((doc) => {
+      console.log(doc);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connected to database successfully');
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 3000;
 
